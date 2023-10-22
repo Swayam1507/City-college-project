2e48783334103368824491a0314db4d971eee121
swayam1507 <s2m0i0t3@gmail.com>
2023-10-22 16:38:55 +0530

    changes

--- backend/controller/users.js
+++ backend/controller/users.js
@@ -1,11 +1,11 @@
-const jwt = require('jsonwebtoken');
-const bcrypt = require('bcrypt');
-const User = require('../models/user'); // Replace with your user model import
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const User = require("../models/user"); // Replace with your user model import
 
-const secretKey = 'your-secret-key';
+const secretKey = "your-secret-key";
 
 const generateToken = (userId) => {
-  return jwt.sign({ userId }, secretKey, { expiresIn: '1h' });
+  return jwt.sign({ userId }, secretKey, { expiresIn: "1h" });
 };
 
 const SignUp = async (req, res) => {
@@ -14,7 +14,7 @@ const SignUp = async (req, res) => {
 
     const existingUser = await User.findOne({ username });
     if (existingUser) {
-      return res.status(400).json({ message: 'User already exists' });
+      return res.status(400).json({ message: "User already exists" });
     }
 
     const hashedPassword = await bcrypt.hash(password, 10);
@@ -25,10 +25,10 @@ const SignUp = async (req, res) => {
     });
     await newUser.save();
 
-    return res.status(201).json({ message: 'User created successfully' });
+    return res.status(201).json({ message: "User created successfully" });
   } catch (error) {
     console.error(error);
-    return res.status(500).json({ message: 'An error occurred' });
+    return res.status(500).json({ message: "An error occurred" });
   }
 };
 
@@ -38,23 +38,25 @@ const SignIn = async (req, res) => {
 
     const user = await User.findOne({ username });
     if (!user) {
-      return res.status(401).json({ message: 'Authentication failed' });
+      return res.status(401).json({ message: "Authentication failed" });
     }
 
     const isPasswordValid = await bcrypt.compare(password, user.password);
     if (!isPasswordValid) {
-      return res.status(401).json({ message: 'Authentication failed' });
+      return res.status(401).json({ message: "Authentication failed" });
     }
 
     const token = generateToken(user._id); // Assuming your user model has an _id field
 
-    return res.status(200).json({ message: 'Authentication successful', token });
+    return res
+      .status(200)
+      .json({ message: "Authentication successful", token });
   } catch (error) {
     console.error(error);
-    return res.status(500).json({ message: 'An error occurred' });
+    return res.status(500).json({ message: "An error occurred" });
   }
 };
 module.exports = {
-    SignIn,
-    SignUp
-}
+  SignIn,
+  SignUp,
+};
