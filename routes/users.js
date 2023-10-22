1dfe52ca6879aea2b423937506a22e98696d6569
swayam1507 <s2m0i0t3@gmail.com>
2023-10-22 16:50:08 +0530

    hello

--- routes/users.js
+++
@@ -1,8 +0,0 @@
-const express = require("express")
-const router = express.Router()
-const{SignIn, SignUp} = require("../controller/users")
-
-router.post('/signup',SignUp)
-router.post('/signin',SignIn)
-
-module.exports = router;
