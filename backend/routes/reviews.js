aa5236f63540bc9a6935ee6a60b2478c5f3fe5ab
swayam1507 <s2m0i0t3@gmail.com>
2023-10-26 22:42:46 +0530

    change in review

--- backend/routes/reviews.js
+++ backend/routes/reviews.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const { create, remove, edit } = require("../controller/reviews");
 
-router.get("/create", create);
+router.post("/create", create);
 router.delete("/delete/:id", remove);
 router.post("/update/:id", edit);
 
